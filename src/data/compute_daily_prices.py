40bc38ad6d9cc1948220425041f029a89a3c4786
Angel David Machado <amachadoa@unal.edu.co>
2022-07-03 18:31:44 -0500

    14

--- src/data/compute_daily_prices.py
+++ src/data/compute_daily_prices.py
@@ -1,5 +1,23 @@
 from operator import index
 from pandas import read_csv
+import pandas as pd
+
+
+def average_daily_price(file):
+    '''
+    Validar test
+    >>> list(average_daily_price(pd.DataFrame({'fecha': ('2021-06-02', '2021-03-02', '2021-06-02', '2021-03-02'),'precio': (12, 40, 12, 80)})).precio)
+    [60.0, 12.0]
+    '''
+    subset_file = file.copy()
+
+    subset_file["fecha"] = pd.to_datetime(subset_file["fecha"])
+    compute_daily_prices = subset_file.groupby(
+        "fecha").mean({"precio": "precio"})
+    compute_daily_prices.reset_index(inplace=True)
+    subset_file = subset_file[["fecha", "precio"]]
+
+    return compute_daily_prices
 
 
 def compute_daily_prices():
@@ -23,6 +41,15 @@ def compute_daily_prices():
     df = df.groupby(df.fecha).mean()
     df = df.iloc[:, 1:2]
     df.to_csv('data_lake/business/precios-diarios.csv')
+
+
+def test_values_compute_daily_prices():
+    df = pd.DataFrame({
+        'fecha': ('2021-06-02', '2021-03-02', '2021-06-02', '2021-03-02'),
+        'precio': (12, 40, 12, 80)
+    })
+    expect = [60.0, 12.0]
+    assert list(average_daily_price(df).precio) == expect
 
 
 if __name__ == "__main__":
